bd38d791a108470062b24361c1b2c6d87bf867b0
gsihaj5 <gsihaj5@gmail.com>
2021-03-26 17:45:24 +0700

    trial count message

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
 
-errorFile=error_message.csv
-userFile=user_statistic.csv
+errorFile="error_message.csv"
+userFile="user_statistic.csv"
 
-echo ERROR,Count > errorFile;
-echo Username,INFO,ERROR > userFile
+echo ERROR,Count > $errorFile
+echo Username,INFO,ERROR > $userFile
 
-getErrorMessage(){
+messages=()
+messageCount=(0 0 0 0 0 0 0 0 0 0)
+
+getErrorMessage() {
     index=0
-    result="";
-    for i in $@
-    do
-        index=$(($index+1));
-        if [ $index -ge 8 ] && [ $index -le $1 ]
-        then
+    result=""
+
+    # shellcheck disable=SC2068
+    for i in $@; do
+        index=$(($index + 1))
+        if [ $index -ge 8 ] && [ $index -le $1 ]; then
             result="${result} ${i}"
         fi
     done
 }
 
-grep ERROR ~/Documents/ITS/sisop/seslab1/_soalShift/soal1/syslog.log | while read line
-do
-    count=`echo "$line" | wc -w`
+appendAndCount() {
+    udahAda=0
+    index=0
 
-    getErrorMessage $count "$line" 
-    echo $result
+    for message in "${messages[@]}"; do
+        if [ "${message}" = "${result}" ]; then
+            udahAda=1
+            break
+        fi
+        index=$(($index + 1))
+    done
+
+    if [ $udahAda -eq 0 ]; then
+        messages+=("${result}")
+    fi
+
+    messageCount[index]=$((${messageCount[index]} + 1))
+}
+
+grep ERROR ~/Documents/ITS/sisop/seslab1/_soalShift/soal1/syslog.log | while read line; do
+    count=$(echo "$line" | wc -w)
+
+    getErrorMessage $count "$line"
+    appendAndCount
+
+    #    for message in "${messages[@]}"
+    #    do
+    #        printf "${message}\n"
+    #    done
+done
+
+echo "$messages"
+index=0
+for message in "${messages[@]}"; do
+    printf "${message}\n"
+    # , ${messageCount[index]}\n"
+    # >> errorFile
+    # index=$(($index + 1));
 done
